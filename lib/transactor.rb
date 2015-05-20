58cd60d154fcb07ad50e331dec098f4320712cbe
Mark Rebec <mark@markrebec.com>
2015-05-20 13:08:34 -0700

    adds gem configuration module attribute/method and narrows down required activesupport libs

--- lib/transactor.rb
+++ lib/transactor.rb
@@ -1,5 +1,6 @@
-require 'active_support/core_ext/string'
-require 'active_support/core_ext/array'
+require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/string/output_safety'
 require 'transactor/version'
 require 'transactor/configuration'
 require 'transactor/errors'
@@ -9,6 +10,13 @@ require 'transactor/improv'
 require 'transactor/transaction'
 
 module Transactor
+  mattr_reader :configuration
+  @@configuration = Transactor::Configuration.new
+
+  def self.configure(&block)
+    @@configuration.configure &block
+  end
+
   def self.transaction(&block)
     Transactor::Transaction.new &block
   end
