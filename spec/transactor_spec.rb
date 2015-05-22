d921af77f0230898883304f8651c691ae63da111
Mark Rebec <mark@markrebec.com>
2015-05-22 15:20:19 -0700

    don't need to test for default config values

--- spec/transactor_spec.rb
+++ spec/transactor_spec.rb
@@ -6,12 +6,4 @@ RSpec.describe Transactor do
       expect(Transactor.transaction).to be_an_instance_of(Transactor::Transaction)
     end
   end
-
-  context 'configuration' do
-    describe 'rollback_failed_actors' do
-      it 'defaults to false' do
-        expect(Transactor.configuration.rollback_failed_actors).to be_false
-      end
-    end
-  end
 end
