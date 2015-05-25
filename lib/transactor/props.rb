5578762b34d39522d01cc48a4160a0a5e662a026
Mark Rebec <mark@markrebec.com>
2015-05-25 12:04:28 -0700

    adds merge methods to props object

--- lib/transactor/props.rb
+++ lib/transactor/props.rb
@@ -2,5 +2,15 @@ require 'ostruct'
 
 module Transactor
   class Props < OpenStruct
+    def merge(hash)
+      self.class.new to_h.merge(hash)
+    end
+
+    def merge!(hash)
+      hash.each do |key, val|
+        self[key] = value
+      end
+      self
+    end
   end
 end
